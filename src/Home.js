2b4310dd1b0e2a3503f99aabad4d1d50bfc16bb1
mdkashif0631 <mdkashif0631@gmail.com>
2025-10-06 06:24:38 +0530

    fix error on test branch

--- src/Home.js
+++ src/Home.js
@@ -6,17 +6,16 @@ import Carousel from './Carousel';
 import PropertySlider from './PropertyCard/PropertySlider';
 import Header from './Header';
 import ContactForm from './About/ContactForm';
-import ProjectConnectDrawer from './ProjectConnectDrawer';
-// import Seo from './Seo';
+import Seo from './Seo';
 
 const Home = () => {
     return (
         <div style={{position:'relative'}}>
-            {/* <Seo project = "Buy, Sell & Rent Properties in Gurgaon | The Luxury Abode Real Estate"
+            <Seo project = "Buy, Sell & Rent Properties in Gurgaon | The Luxury Abode Real Estate"
             desc = "Find the best residential, commercial, and plot properties in Gurgaon with The Luxury Abode. Explore verified listings, new projects, and investment opportunities today."
             img= "https://res.cloudinary.com/daa1hgr9j/image/upload/v1757927534/g62fxethsjgbay1shszv.jpg"
             link= ""
-            /> */}
+            />
             <HeroSection />
             <Header />
             <PropertySlider />
@@ -24,7 +23,6 @@ const Home = () => {
             <CityListings />
             <Carousel />
             <ContactForm/>
-            <ProjectConnectDrawer/>
         </div>
     )
 }
